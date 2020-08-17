1650069a0a47b2006182d3574c490a8f84424e0c
hunghnuit <hunghn0999@gmail.com>
2020-08-18 00:18:23 +0700

    Add header for requests due to new changes of instagram (tool is working at 8/18/2020)

--- insta_down_tool.py
+++ insta_down_tool.py
@@ -10,6 +10,8 @@ path = "./InstaDown/" #Change directory saving pics here
 query_hash_for_user = "9dcf6e1a98bc7f6e92953d5a61027b98"  # Case 1
 query_hash_for_specific_post = "a92f76e852576a71d3d7cef4c033a95e"  # case 2:
 
+headers = {'cookie': 'sessionid=5711537494%3AsqShow1gFBehyH%3A22;',
+'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36'}
 
 def get_specific_post(shortCode):
     global i, path
@@ -18,7 +20,7 @@ def get_specific_post(shortCode):
         query_hash_for_specific_post, shortCode)
 
     # Now we use get method to request and load what we got into json_data
-    response = requests.get(query_hash_url)
+    response = requests.get(query_hash_url, headers=headers)
     json_data = json.loads(response.text)
 
     try:
@@ -57,7 +59,7 @@ def get_all_post_of_user():
     input_url = input()
 
     url_step1 = input_url + "?__a=1"
-    json_data_step1 = requests.get(url_step1).json()
+    json_data_step1 = requests.get(url_step1, headers=headers).json()
     id_user = json_data_step1['graphql']['user']['id']
     # print(id_user)
 
@@ -65,7 +67,7 @@ def get_all_post_of_user():
     while (end_cursor != None):
         url_step2 = "https://www.instagram.com/graphql/query/?query_hash={}&variables=%7B%22id%22%3A%22{}%22%2C%22first%22%3A{}%2C%22after%22%3A%22{}%22%7D".format(
             query_hash_for_user, id_user, default_first, end_cursor)
-        json_data_step2 = requests.get(url_step2).json()
+        json_data_step2 = requests.get(url_step2, headers=headers).json()
 
         # print(url_step2)
 
